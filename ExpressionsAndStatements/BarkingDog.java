3c6912d9d6481097ef6ad4b0e2e19a95e44d6493
Stiliyan26 <stili262002@abv.bg>
2023-06-23 18:17:18 +0300

    done

---
+++ ExpressionsAndStatements/BarkingDog.java
@@ -0,0 +1,15 @@
+package ExpressionsAndStatements;
+public class BarkingDog {
+    public static void main(String[] args) {
+        shouldWakeUp (true, 1);
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if (hourOfDay < 0 || 23 < hourOfDay){
+            return  false;
+        } else if ((hourOfDay < 8 || hourOfDay > 22) && barking) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ ExpressionsAndStatements/SpeedConverter.java
@@ -0,0 +1,27 @@
+package ExpressionsAndStatements;
+public class SpeedConverter {
+    public static void main(String[] args) {
+        long miles = toMilesPerHour(10.25);
+
+        System.out.println("The number is " + miles);
+        printConversion(-2.0);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour * 0.621371);
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = Math.round(kilometersPerHour * 0.621371);
+
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
