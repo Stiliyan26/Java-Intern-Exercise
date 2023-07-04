c90c8f858fee2ca3334d63a5428ab3c583d1d503
Stiliyan26 <stili262002@abv.bg>
2023-07-04 23:38:07 +0300

    POJO

---
+++ OOP_Inheritance/POJO.java
@@ -0,0 +1,21 @@
+package OOP_Inheritance;
+public class POJO {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++){
+            Student student = new Student("S92300" + i,
+                    switch(i){
+                        case 1 -> "Kiro";
+                        case 2 -> "Mirko";
+                        case 3 -> "Jozi";
+                        case 4 -> "Peter";
+                        case 5 -> "Hrisi";
+                        default -> "Annonymous";
+                    },
+                    "05/11/2002",
+                    "Java MasterClass");
+
+            System.out.println(student);
+        }
+    }
+}
